36b6ff5b06f19a70653029eca7ef449db6761a80
dominictosku <dominic.tosku@hotmail.com>
2024-11-28 10:11:55 +0100

    init

---
+++ m324-npm/index.js
@@ -0,0 +1,8 @@
+import chalk from "chalk";
+import { generate } from "random-words";
+
+const word1 = generate({ exactly: 1, minLength: 10, wordsPerString: 1 });
+const word2 = generate();
+console.log(
+  `The ${chalk.greenBright(word1)} is ${chalk.redBright(word2)}.`
+);
