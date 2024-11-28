17e859a1480ca7d29ee05eceb0027f88edbbcfa9
dominictosku <dominic.tosku@hotmail.com>
2024-11-28 10:59:15 +0100

    feat: add more tests

--- m324-npm/index.test.js
+++ m324-npm/index.test.js
@@ -4,6 +4,16 @@ import { sum } from "./index.js";
 describe("sum", () => {
   it("should return 3 for 1 + 2", () => {
     // Korrigieren Sie den Test
-    assert.equal(sum(1, 2), 4);
+    assert.equal(sum(1, 2), 3);
+  });
+
+  it("should return 4 for 2 + 2", () => {
+    // Korrigieren Sie den Test
+    assert.equal(sum(2, 2), 4);
+  });
+
+  it("should return 4.5 for 2.5 + 2", () => {
+    // Korrigieren Sie den Test
+    assert.equal(sum(2.5, 2), 4.5);
   });
 });
