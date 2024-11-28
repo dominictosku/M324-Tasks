8332295cf36d9179be5654c44877756e5ebfd75c
dominictosku <dominic.tosku@hotmail.com>
2024-11-28 10:43:30 +0100

    feat: add prettier

---
+++ m324-npm/eslint.config.js
@@ -0,0 +1,8 @@
+// eslint.config.js
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+]
--- m324-npm/index.js
+++ m324-npm/index.js
@@ -1,8 +1,6 @@
-import chalk from "chalk";
-import { generate } from "random-words";
+import chalk from 'chalk'
+import { generate } from 'random-words'
 
-const word1 = generate({ exactly: 1, minLength: 10, wordsPerString: 1 });
-const word2 = generate();
-console.log(
-  `The ${chalk.greenBright(word1)} is ${chalk.redBright(word2)}.`
-);
+const word1 = generate({ exactly: 1, minLength: 10, wordsPerString: 1 })
+const word2 = generate()
+console.log(`The ${chalk.greenBright(word1)} is ${chalk.redBright(word2)}.`)
